c12fa8dfcb5f08b6f2663e273f9871c3c6e52243
rajan BM <orajanbm@gamil.com>
2024-11-15 12:06:39 +0545

    my first commit

---
+++ foo_plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package foo_plugin
+ */
+/*
+Plugin Name: Foo Plugin
+Plugin URI: http://example.com/foo-plugin
+Description: A plugin that does foo.
+Version: 1.0
+Author: Firstname Lastname
+Author URI: http://example.com
+License: GPL2
+*/
